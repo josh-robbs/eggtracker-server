3556b669ee840fd6e526e080ecaeb88d2eabae88
Josh Robbs <joshrobbs@Zuul.local>
2018-09-15 16:56:01 -0600

    server up and running

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const bodyParser = require("body-parser")
+const port = process.env.PORT || 8080
+
+
+app.use(cors())
+app.use(bodyParser.json())
+
+app.get('/', (req,res,next) => {  
+  res.json({
+      message: 'Hello World!'
+  }); 
+})
+
+app.listen(port, () => {
+    console.log(`I am listening on ${port}`)
+})
